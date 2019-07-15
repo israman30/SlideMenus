55d46f20cef12d1ded163c8c050ff3b16f4eb93e
Israel Manzo <israelmanzo814@gmail.com>
2019-07-15 10:45:38 -0400

    Added buttons for menus

--- MenuSlide/MenuSlide/MainController.swift
+++ MenuSlide/MenuSlide/MainController.swift
@@ -31,7 +31,39 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setMainView()
+        setMenuButtons()
     }
+    
+    func setMenuButtons() {
+        
+        let leftMenuButton = UIButton(type: .system)
+        leftMenuButton.setImage(#imageLiteral(resourceName: "left"), for: .normal)
+        leftMenuButton.addTarget(self, action: #selector(handleLeftMenu), for: .touchUpInside)
+        
+        let rightMenuButton = UIButton(type: .system)
+        rightMenuButton.setImage(#imageLiteral(resourceName: "right"), for: .normal)
+        rightMenuButton.addTarget(self, action: #selector(handleRightMenu), for: .touchUpInside)
+        
+        view.addSubViews(leftMenuButton, rightMenuButton)
+        
+        leftMenuButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, padding: .init(top: 10, left: 15, bottom: 0, right: 0), size: .init(width: 30, height: 30))
+        rightMenuButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 15), size: .init(width: 20, height: 30))
+    }
+    
+    @objc func handleLeftMenu(){
+        print("Left button")
+    }
+    
+    @objc func handleRightMenu(){
+        print("right button")
+    }
+    
+    
+
+
+}
+
+extension MainController {
     
     func setMainView() {
         view.backgroundColor = .white
@@ -44,7 +76,5 @@ class MainController: UIViewController {
         authorLabel.centerInSuperview(size: .init(width: view.frame.width, height: 30), constantY: 200)
     }
     
-
-
 }
 
